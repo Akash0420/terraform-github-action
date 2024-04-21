d5902196dfd355094baeb1dba06e449bbed3589b
Akash0420 <menonakash0420@gmail.com>
2024-04-21 18:36:10 +0530

    tf workflow updated4

--- src/terraform/main.tf
+++ src/terraform/main.tf
@@ -7,8 +7,8 @@ terraform {
   }
 
  backend "azurerm" {
-    resource_group_name  = "friday-demo-rg"
-    storage_account_name = "sttfstatemgt01"
+    resource_group_name  = "cloud-shell-storage-centralindia"
+    storage_account_name = "csg10032003730c9146"
     container_name       = "tfstate"
     key                  = "demo.terraform.tfstate"
   }
